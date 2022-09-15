1a576ce83ad1d412e3cb578a33241a9362849e11
JaeYeol <woduf0628@naver.com>
2022-09-16 00:36:10 +0900

    complete star

--- 2week/pa01_star.cpp
+++ 2week/pa01_star.cpp
@@ -2,73 +2,61 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
-vector<vector<int>> getSkyline(vector<vector<int>>& buildings){
-    
-    /*
-        [start, -height, validUntil]
-        height > 0: start edge
-        height == 0: end edge
-    */
+vector< vector<int> > getSkyline(vector< vector<int> >& buildings){
 
-    vector<vector<int>> edges;
-    for(int i = 0; i < buildings.size(); i++){
+    vector< vector<int> > edges;
+    for(auto building : buildings){
         int x = edges.size();
         edges.push_back(vector<int>());
-        edges[x].push_back(buildings[i][0]);
-        edges[x].push_back(-buildings[i][2]);
-        edges[x].push_back(buildings[i][1]);
+        edges[x].push_back(building[0]);
+        edges[x].push_back(-building[2]);
+        edges[x].push_back(building[1]);
     }
 
-    for(int i = 0; i < buildings.size(); i++){
+    for(auto building : buildings){
         int x = edges.size();
         edges.push_back(vector<int>());
-        edges[x].push_back(buildings[i][1]);
+        edges[x].push_back(building[1]);
         edges[x].push_back(0);
-        edges[x].push_back(1e9);
+        edges[x].push_back(1024);
     }
 
     sort(edges.begin(), edges.end());
 
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> prevHighest;
+    priority_queue< pair<int,int>, vector< pair<int,int> >, greater< pair<int,int> > > preHigher;
 
-    prevHighest.push({0, 1e9});
+    preHigher.push(make_pair(0, 1024));
 
-    vector<vector<int>> skyline;
+    vector< vector<int> > skyline;
 
-    for(int i = 0; i < edges.size(); i++){
+    for(auto edge : edges){
         
-        int position = edges[i][0];
-        int curHeight = -1*edges[i][1];
-        int validUntil = edges[i][2];
+        int pos = edge[0];
+        int curHeight = -1*edge[1];
+        int isValid = edge[2];
 
-        while(prevHighest.top().second <= position){
-            /*
-                The equal term guarantees that, for end edges, popping 
-                from height to low still works.
-            */
-            prevHighest.pop();
+        while(preHigher.top().second <= pos){
+            preHigher.pop();
         }
         
         if(curHeight > 0){
-            // Start edge.
-            prevHighest.push({-curHeight, validUntil});
+            preHigher.push(make_pair(-curHeight, isValid));
         }
         
-        // prevHighest is now the current highest.
-
         if(skyline.size() == 0){
             skyline.push_back(vector<int>());
-            skyline[0].push_back(position);
-            skyline[0].push_back(-prevHighest.top().first);
+            skyline[0].push_back(pos);
+            skyline[0].push_back(-preHigher.top().first);
         }
-        else if(skyline.back()[1] != -prevHighest.top().first){
+        else if(skyline.back()[1] != -preHigher.top().first){
             int x = skyline.size();
             skyline.push_back(vector<int>());
-            skyline[x].push_back(position);
-            skyline[x].push_back(-prevHighest.top().first);   
+            skyline[x].push_back(pos);
+            skyline[x].push_back(-preHigher.top().first);   
         }
     }
 
@@ -78,27 +66,60 @@ vector<vector<int>> getSkyline(vector<vector<int>>& buildings){
 int main(){
 
     int nBuilding=0, nStar=0;
-    vector<vector<int>> bPositions;
-    vector<int> sPositions;
+    vector< vector<int> > buildings, stars;
+
+    cin >> nBuilding;
 
     for(int i=0; i<nBuilding; i++){
         int xPos=0, yPos=0, hPos=0;
-        cin >> xPos >> yPos >> hPos;
+        cin >> xPos >> hPos >> yPos;
         vector<int> pos;
         pos.push_back(xPos);
-        pos.push_back(yPos);
+        pos.push_back(xPos+yPos);
         pos.push_back(hPos);
-        bPositions.push_back(pos);
+        
+        buildings.push_back(pos);
     }
+
+    cin >> nStar;
 
     for(int i=0; i<nStar; i++){
         int xPos=0, yPos=0;
         cin >> xPos >> yPos;
-        sPositions.push_back(xPos);
-        sPositions.push_back(yPos);
+        vector<int> star;
+        star.push_back(xPos);
+        star.push_back(yPos);
+
+        stars.push_back(star);
     }
 
-    vector<vector<int>> skyline = getSkyline(bPositions);
+    vector< vector<int> > skyline = getSkyline(buildings);
+
+    for(auto star: stars){
+        int starX = star[0], starY=star[1], i;
+        int cmpX1 = 0, cmpY1 = 0, cmpX2 = 0, cmpY2 = 0;
+
+        for(i=0; skyline[i][0] < starX && i < skyline.size(); i++){
+            cmpX1 = skyline[i][0];
+            cmpY1 = skyline[i][1];
+        }
+        cmpX2 = skyline[i][0];
+        cmpY2 = skyline[i][1];
+
+        if(starX == cmpX2 && cmpY1 < cmpY2){
+            if(starY > cmpY2){ cout << "over" << endl;}
+            else if(starY <= cmpY2 && starY >= cmpY1){ cout << "on" << endl;}
+            else{ cout << "under" << endl;}
+        }else if(starX == cmpX2 && cmpY1 > cmpY2){
+            if(starY > cmpY1){ cout << "over" << endl;}
+            else if(starY <= cmpY1 && starY >= cmpY2){ cout << "on" << endl;}
+            else{ cout << "under" << endl;}
+        }else{
+            if(starY > cmpY1){ cout << "over" << endl;}
+            else if(starY < cmpY1){ cout << "under" << endl;}
+            else{ cout << "on" << endl;}
+        }
+    }
 
     return 0;
 }
