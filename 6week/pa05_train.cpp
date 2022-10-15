5ea6f0bad9b104d62a45899162e020119c934937
JaeYeol <woduf0628@naver.com>
2022-10-15 14:34:08 +0900

    complete

--- 6week/pa05_train.cpp
+++ 6week/pa05_train.cpp
@@ -1,95 +1,64 @@
 #include <iostream>
-#include <stack>
-#include <algorithm>
 #include <vector>
-
+#include <list>
 using namespace std;
 
+vector<int> weight;
+
+int line(int index, list<int> li, int B, int add){
+    int cnt = 0, tmp;
+    if(add != -1){
+        if(add < li.front()) li.push_front(add);
+        else if(li.back() < add) li.push_back(add);
+        else return li.size();
+    }
+
+    for(int i=index; i<weight.size(); i++){
+        if(li.front() < weight[i] && weight[i] < li.back() &&
+            li.front() < B && B < li.back()) return li.size();
+
+        if(weight[i] < B && B < li.front()){
+            li.push_front(B);
+            B = weight[i];
+        } else if(B < weight[i] && weight[i] < li.front()){
+            li.push_front(weight[i]);
+        } else if(li.back() < B && B < weight[i]){
+            li.push_back(B);
+            B = weight[i];
+        } else if(li.back() < weight[i] && weight[i] < B){
+            li.push_back(weight[i]);
+        } else {
+            tmp = line(i+1, li, weight[i], B);
+            cnt = max(tmp, cnt);
+            tmp = line(i+1, li, B, weight[i]);
+            cnt = max(tmp, cnt);
+            return cnt;
+        }
+    }
+
+    if(li.front() < B && B < li.back()) return li.size();
+    else return li.size() + 1;
+}
+
 int main(){
-    int n;
-    cin >> n;
-
-    stack<int> tmp;
-    vector<int> train; //front가 큰 값
-    for(int i = 0; i < n; i++){
-        int weight;
-        cin >> weight;
-
-        cout << "=============train start===============" << endl;
-        for(int i : train){
-            cout << i << " ";
-        }
-        cout << endl;
-        cout << "==============train end==============" << endl;
-
-        if(train.size() == 0 && tmp.size() == 0){
-            if(weight != 1 || weight != n){
-                train.push_back(weight);
-            }else{
-                tmp.push(weight);
-            }
-        }else if(train.size() == 0 && tmp.size() != 0){
-            if(weight != 1 || weight != n){
-                train.push_back(weight);
-            }else{
-                train.push_back(tmp.top());
-                tmp.pop();
-                tmp.push(weight);
-            }
-        }else{
-            if(tmp.size() == 0){
-                if(train.back() - weight == 1){
-                    train.push_back(weight);
-                }else if(weight - train.front() == 1){
-                    train.insert(train.begin(), weight);
-                }else{
-                    tmp.push(weight);
-                }
-            }else{
-                int cmp = tmp.top();
-                if(cmp > train.front() && weight > train.front()){
-                    if(cmp < weight){
-                        train.push_back(weight);
-                    }else{
-                        train.push_back(cmp);
-                        tmp.pop();
-                        tmp.push(weight);
-                    }
-                }else if(cmp > train.front() && weight < train.front()){
-                    if(cmp - train.front() < train.back() - weight){
-                        train.insert(train.begin(), cmp);
-                        tmp.pop();
-                        tmp.push(weight);
-                    }else{
-                        train.push_back(weight);
-                    }
-                }else if(cmp < train.front() < 0 && weight > train.front()){
-                    if(cmp - train.back() < train.front() - weight){
-                        train.push_back(cmp);
-                        tmp.pop();
-                        tmp.push(weight);
-                    }else{
-                        train.insert(train.begin(), weight);
-                    }
-                }else{
-                    if(cmp > weight){
-                        train.push_back(cmp);
-                        tmp.pop();
-                        tmp.push(weight);
-                    }else{
-                        train.push_back(weight);
-                    }
-                }
-            }
-        }
+    int N; cin >> N;
+    int tmp;
+    while(N--){
+        cin >> tmp;
+        weight.push_back(tmp);
     }
-
-    for(int i : train){
-        cout << i << " ";
+    if(weight.size() < 2){
+        cout << weight.size();
+        return 0;
     }
-    
-    //cout << train.size() << endl;
+    list<int> li;
+    li.push_back(weight[0]);
+    int result = 0;
+    result = max(line(2, li, weight[1], -1), result);
+    li.clear();
+    li.push_back(weight[1]);
+    result = max(line(2, li, weight[0], -1), result);
 
+    cout << result;
     return 0;
-    
 }
