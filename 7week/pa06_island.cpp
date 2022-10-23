7b5596a3c2e90205a5244582ae7705679810655a
DESKTOP-687CVLP\newdobby <woduf0628@naver.com>
2022-10-23 23:56:59 +0900

    fix

--- 7week/pa06_island.cpp
+++ 7week/pa06_island.cpp
@@ -1,50 +1,46 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+
 #define MAX 801
 
 using namespace std;
 
-int N, M, sum=0;                       // 미로 크기
-int map[MAX][MAX];             // 미로 표현용 2차원 배열
-int visited[MAX][MAX];          // 방문 기록용 2차원 배열
-int dist[MAX][MAX];             // 이동칸 기록용 2차원 배열
+int n, m, sum=0;                      
+int map[MAX][MAX];             
+int visited[MAX][MAX];          
+int dist[MAX][MAX];             
 
-int x_dir[4] = {-1, 1, 0, 0};   // 상화좌우 x축 방향
-int y_dir[4] = {0, 0, -1, 1};   // 상화좌우 y축 방향
+int xDir[4] = {-1, 1, 0, 0};   
+int yDir[4] = {0, 0, -1, 1};   
 
-queue<pair<int,int> > q;        // 탐색 좌표 저장용 queue
+queue<pair<int,int> > q;       
 
-// 미로 경로 탐색
-void bfs(int start_x, int start_y){         
 
-    visited[start_y][start_x] = 1;          // 입력 받은 시작 좌표를 방문
-    q.push(make_pair(start_x,start_y));     // queue 에 삽입
-    dist[start_y][start_x]++;               // 시작 좌표까지 이동한 칸을 1로 지정
+void bfs(int startX, int startY){         
 
-    // 모든 좌표를 탐색할 때까지 반복
+    visited[startY][startX] = 1;          
+    q.push(make_pair(startX,startY));    
+    dist[startY][startX]++;              
+
     while (!q.empty()){
 
-        // queue 의 front 좌표를, 현재 좌표로 지정
         int x = q.front().first;
         int y = q.front().second;
 
-        // qeueu 의 front 좌표 제거
         q.pop();
 
-        // 현재 좌표와 상하좌우로 인접한 좌표 확인
         for (int i=0; i<4; ++i){
 
-            // 현재 좌표와 상하좌우로 인접한 좌표
-            int x_new = x + x_dir[i];
-            int y_new = y + y_dir[i];
+            int xNew = x + xDir[i];
+            int yNew = y + yDir[i];
 
-            // 인접한 좌표가, 미로 내에 존재하는지, 방문한 적이 없는지, 이동이 가능한지 확인
-            if ( (0 <= x_new && x_new < M) && (0 <= y_new && y_new < N) 
-            && !visited[y_new][x_new] && map[y_new][x_new] == 0 ){
+            if ( (0 <= xNew && xNew < m) && (0 <= yNew && yNew < n) 
+            && !visited[yNew][xNew] && map[yNew][xNew] == 0 ){
 
-                visited[y_new][x_new] = 1;              // 인접 좌표는 방문한 것으로 저장
-                q.push(make_pair(x_new, y_new));        // 인접 좌표를 queue 에 삽입
-                dist[y_new][x_new] = dist[y][x] + 1;    // 인접 좌표까지 이동한 거리 저장
+                visited[yNew][xNew] = 1;             
+                q.push(make_pair(xNew, yNew));        
+                dist[yNew][xNew] = dist[y][x] + 1;    
             }
         }
         sum++;
@@ -53,7 +49,7 @@ void bfs(int start_x, int start_y){
 
 int main(){
 
-    cin >> M >> N;                      
+    cin >> m >> n;                      
 
     vector<string> arr;
     string str;
@@ -64,7 +60,7 @@ int main(){
         /* code */
     } while (str != "-1");
 
-    int *l = new int[M*N];
+    int *l = new int[m*n];
     int k = 0;
     for(int i=0; i<arr.size(); i++){
         if(arr[i] == "-1") break;
@@ -75,15 +71,15 @@ int main(){
     }
 
     k=0;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j< M; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j< m; j++){
             map[i][j] = l[k];
             k++;
         }
 
     }
 
-    bfs(0, 0);                          // 미로 탐색 시작
+    bfs(0, 0);                          
 
-    cout << M*N-sum << "\n";             // 도착 좌표까지의 거리 출력
+    cout << m*n-sum << "\n";             
 }
