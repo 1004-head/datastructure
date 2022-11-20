78a7b368debadeefaefa034b9421495db69e4fe9
JaeYeol <woduf0628@naver.com>
2022-11-20 14:25:47 +0900

    lab10

--- lab10/pa10_mafia.cpp
+++ lab10/pa10_mafia.cpp
@@ -15,22 +15,58 @@ typedef struct node{
 }Node;
 
 int N;
-vector<pair<string, string>> inv;
-map<string, Node> m;
+vector< pair<string, string> > inv;
+map<string, Node> M;
+map<string, vector<string> > m2;
+vector<string> mafia;
+
+string findRoot(vector< pair<string, string> > &v){
+    string root = v[0].second;
+    for(int i = 0; i<N; i++){
+        if(inv[i].first == root) root = inv[i].second;
+    }
+    return root;
+}
+
+int childCtr(string k){
+    if(m2.find(k) == m2.end()) return 1;
+    return 
+}
 
 int main(){
     cin >> N;
 
-    for(int i=0; i<N; i++){
+    for(int i=0; i<N-1; i++){
         string a, b;
         cin >> a >> b;
 
-        inv.push_back({a,b});
+        inv.push_back(make_pair(a,b));
+        if(m2.count(b) == 0){
+            vector<string> tmp;
+            tmp.push_back(a);
+            m2.insert(make_pair(b, tmp));
+        }else{
+            vector<string> tmp;
+            m2.find(b)->second.push_back(a);
+        }
+    }
+    
+    for(map<string, vector<string> >::iterator it = m2.begin(); it != m2.end(); it++){
+        cout << "===========================" << endl;
+        cout << it->first << endl;
+        for(vector<string>::iterator it2 = it->second.begin(); it2 != it->second.end(); it2++){
+            cout << *it2 << " ";
+        }
+        cout << endl;
     }
 
-    string root = inv[0].second;
-    for(int i = 0; i<N; i++){
-        if(inv[i].first == root) root = inv[i].second;
+    string root = findRoot(inv);
+    mafia.push_back(root);
+    int n = m2[root].size();
+
+    for(int i = 0; i < n; i++){
+        int k = childCtr(m2[root][i]);
+        cout << k << endl;
     }
 
     cout << root << endl;
