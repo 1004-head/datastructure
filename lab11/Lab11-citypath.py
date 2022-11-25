5f76ea05541caaeb7ab3f280c4ff077f496da452
1004-head <woduf0628@naver.com>
2022-11-25 16:09:15 +0900

    lab11 dijkstra

--- lab11/Lab11-citypath.py
+++ lab11/Lab11-citypath.py
@@ -1,23 +1,22 @@
 import heapq
 
 def dijkstra(graph, start):
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0
-    pq = [(0, start)]
+    distances = {node: float('inf') for node in graph}  
+    distances[start] = 0  
     queue = []
-    heapq.heappush(queue, [distances[start], start])
+    heapq.heappush(queue, [distances[start], start])  
 
-    while queue:
-        current_distance, current_destination = heapq.heappop(queue)
+    while queue:  
+        current_distance, current_destination = heapq.heappop(queue)  
 
-        if distances[current_destination] < current_distance:
+        if distances[current_destination] < current_distance:  
             continue
-
+    
         for new_destination, new_distance in graph[current_destination].items():
-            distance = current_distance + new_distance
-            if distance < distances[new_destination]:
-                distances[new_destination] = distance
-                heapq.heappush(queue, [distances, new_destination])
+            distance = current_distance + new_distance  
+            if distance < distances[new_destination]:  
+              distances[new_destination] = distance
+              heapq.heappush(queue, [distance, new_destination])  
     
     return distances
 
@@ -44,9 +43,13 @@ for key, val in m.items():
 
 E = len(e)
 
-graph = {v1: {v2 : len(m[v2])-2 for v2 in m[v1]} for v1 in m.keys()}
+graph = {v1: {v2 : len(m[v2])-1 for v2 in m[v1]} for v1 in m.keys()}
 
 for _ in e:
     print(f"v1={_[0]}, v2={_[1]}")
 
-print(dijkstra(graph, 1))
+a = list()
+for i in range(1, N-1):
+    a.append(max(dijkstra(graph, i).values()))
+
+print(max(a))
